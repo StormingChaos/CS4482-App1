0f16db8a8be35ec258df7d6cc7be6cd5da1afb74
Ariya <57851464+StormingChaos@users.noreply.github.com>
2021-09-27 17:49:50 -0400

    Created Zombunny
    - Completed 4th tutorial of Survival Shooter: Creating Enemy #1
    - also messed around with navmesh values to create a more accurate bake

---
+++ CS4482 App1/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {*/
+            nav.SetDestination(player.position);/*
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+        */
+    }
+}
