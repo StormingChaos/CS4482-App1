ab3954d436b8605048bc3e1c1daef9f4820412c6
Ariya <57851464+StormingChaos@users.noreply.github.com>
2021-09-29 15:31:34 -0400

    Spawning Enemies
    - Completed Tutorial 9

--- CS4482 App1/Assets/Scripts/Enemy/EnemyMovement.cs
+++ CS4482 App1/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,14 +5,15 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
-    NavMeshAgent nav;
+    Transform player;                   // Reference to the player's position.
+    PlayerHealth playerHealth;          // Reference to the player's health.
+    EnemyHealth enemyHealth;            // Reference to this enemy's health.
+    NavMeshAgent nav;                   // Reference to the nav mesh agent.
 
     // Start is called before the first frame update
     void Awake()
     {
+        // Set up the references.
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
@@ -22,12 +23,16 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If the enemy and the player have health left...
         if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
+            // ... set the destination of the nav mesh agent to the player.
             nav.SetDestination(player.position);
         }
+        // Otherwise...
         else
         {
+            // ... disable the nav mesh agent.
             nav.enabled = false;
         }
         
--- CS4482 App1/Assets/Scripts/Managers/ScoreManager.cs
+++ CS4482 App1/Assets/Scripts/Managers/ScoreManager.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int score;
-    Text text;
+    public static int score;       // The player's score.
+    Text text;                     // Reference to the Text component.
 
     void Awake()
     {
+        // Set up the reference.
         text = GetComponent<Text>();
+        // Reset the score.
         score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Set the displayed text to be the word "Score" followed by the score value.
         text.text = "Score: " + score;
     }
 }
